46f82b7255b9422f3272d576935e9bc6606911bb
Deepa Nagaraj <dnagaraj@google.com>
2020-02-20 12:17:18 +0530

    Implemented Users Tab.

--- app/src/main/java/com/dnagaraj/myapplication/ProfileTab.java
+++ app/src/main/java/com/dnagaraj/myapplication/ProfileTab.java
@@ -7,58 +7,94 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileTab#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+
 public class ProfileTab extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private EditText etProfileName,etBio,etProfession,etHobbies,etFavSports;
+    Button btnUpdateInfo;
 
     public ProfileTab() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileTab.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileTab newInstance(String param1, String param2) {
-        ProfileTab fragment = new ProfileTab();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        View view= inflater.inflate(R.layout.fragment_profile_tab, container, false);
+
+        etProfileName=view.findViewById(R.id.etProfileName);
+        etBio=view.findViewById(R.id.etBio);
+        etProfession=view.findViewById(R.id.etProfession);
+        etHobbies=view.findViewById(R.id.etHobbies);
+        etFavSports=view.findViewById(R.id.etFavSport);
+
+        btnUpdateInfo=view.findViewById(R.id.btnUpdateInfo);
+
+        final ParseUser parseUser=ParseUser.getCurrentUser();
+
+        if(parseUser.get("ProfileName")==null){
+            etProfileName.setText("");
+        }else {
+            etProfileName.setText(parseUser.get("ProfileName").toString());
+        }
+
+        if(parseUser.get("Bio")==null){
+            etBio.setText("");
+        }else {
+            etBio.setText(parseUser.get("Bio").toString());
+        }
+
+        if(parseUser.get("Profession")==null){
+            etProfession.setText("");
+        }else {
+            etProfession.setText(parseUser.get("Profession").toString());
+        }
+
+        if(parseUser.get("Hobbies")==null){
+            etHobbies.setText("");
+        }else {
+            etHobbies.setText(parseUser.get("Hobbies").toString());
+        }
+
+        if(parseUser.get("FavSports")==null){
+            etFavSports.setText("");
+        }else {
+            etFavSports.setText(parseUser.get("FavSports").toString());
+        }
+
+        btnUpdateInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                parseUser.put("ProfileName",etProfileName.getText().toString());
+                parseUser.put("Bio",etBio.getText().toString());
+                parseUser.put("Profession",etProfession.getText().toString());
+                parseUser.put("Hobbies",etHobbies.getText().toString());
+                parseUser.put("FavSports",etFavSports.getText().toString());
+
+                parseUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e==null){
+                            FancyToast.makeText(getActivity(), "Info saved successfully",
+                                    FancyToast.LENGTH_SHORT, FancyToast.SUCCESS, true).show();
+                        }else{
+                            FancyToast.makeText(getActivity(), e.getMessage(),
+                                    FancyToast.LENGTH_SHORT, FancyToast.ERROR, true).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        return  view;
     }
 }
